fc140de3ebae9526083b5bf924d9fc126fe960fd
zumuvik <toovalvedota2@gmail.com>
2026-03-21 23:23:51 +0300

    Auto commit before nixos-rebuild: 2026-03-21 23:23:51

--- home.nix
+++ home.nix
@@ -99,11 +99,7 @@
 
   programs.vscode = {
     enable = true;
-    profiles.default.extensions = with pkgs.vscode-extensions; [
-      dracula-theme.theme-dracula
-      vscodevim.vim
-      yzhang.markdown-all-in-one
-    ];
+    package = pkgs.vscodium.fhs;
   };
 
   programs.ags = {
