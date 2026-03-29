775f84c20ff6044bbf49817f7135a43ee8b8f693
zumuvik <weterantip@gmail.com>
2026-03-29 22:48:56 +0300

    feat: add auto-login to greetd
    - initial_session: auto-login with Hyprland for username
    - default_session: tuigreet fallback when exiting session

--- modules/system/greetd.nix
+++ modules/system/greetd.nix
@@ -1,9 +1,16 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, username, ... }:
 
 {
   services.greetd = {
     enable = true;
     settings = {
+      # Автологин с Hyprland
+      initial_session = {
+        command = "Hyprland";
+        user = username;
+      };
+
+      # Fallback на tuigreet (если выйдешь из сессии)
       default_session = {
         command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd Hyprland --remember --remember-user-session";
         user = "greeter";
