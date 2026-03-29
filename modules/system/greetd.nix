f963012ad15f36fca2b7bbe91a7e89608d18abfc
zumuvik <weterantip@gmail.com>
2026-03-29 22:56:47 +0300

    fix: remove auto-login from greetd

--- modules/system/greetd.nix
+++ modules/system/greetd.nix
@@ -1,16 +1,9 @@
-{ lib, pkgs, username, ... }:
+{ lib, pkgs, ... }:
 
 {
   services.greetd = {
     enable = true;
     settings = {
-      # Автологин с Hyprland
-      initial_session = {
-        command = "Hyprland";
-        user = username;
-      };
-
-      # Fallback на tuigreet (если выйдешь из сессии)
       default_session = {
         command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd Hyprland --remember --remember-user-session";
         user = "greeter";
