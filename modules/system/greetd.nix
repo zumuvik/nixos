08902fc66128220efda42ee0771128b607c00a73
zumuvik <weterantip@gmail.com>
2026-03-29 13:55:13 +0300

    fix: switch from regreet to tuigreet
    - regreet doesn't see Hyprland sessions (no .desktop file)
    - tuigreet: simple TUI, --cmd Hyprland directly
    - --remember --remember-user-session for convenience

--- modules/system/greetd.nix
+++ modules/system/greetd.nix
@@ -5,23 +5,8 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.cage}/bin/cage -s -- ${pkgs.regreet}/bin/regreet";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd Hyprland --remember --remember-user-session";
         user = "greeter";
-      };
-    };
-  };
-
-  programs.regreet = {
-    enable = true;
-    settings = {
-      appearance = {
-        greeting_msg = "Welcome back";
-      };
-      GTK = {
-        application_prefer_dark_theme = lib.mkForce true;
-        cursor_theme_name = lib.mkForce "Bibata-Modern-Classic";
-        font_name = lib.mkForce "JetBrains Mono";
-        theme_name = lib.mkForce "Adwaita-dark";
       };
     };
   };
