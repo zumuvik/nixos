ec3ad917da2089b67f9a010e56b19193dc2f3ad1
zumuvik <weterantip@gmail.com>
2026-03-29 10:54:15 +0300

    refactor: split hyprland config and fix imports
    - modules/home/hyprland/scripts.nix: move home.file exports
    - modules/home/hyprland/exec-once.nix: move exec-once settings
    - modules/home/hyprland/default.nix: import new modules
    - home.nix: remove duplicate hyprlock.conf import
    - modules/home/hyprland/hyprland.nix: add hyprlock.conf as import

---
+++ modules/home/hyprland/exec-once.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, ... }:
+
+{
+  wayland.windowManager.hyprland.settings = {
+     "$sD" = "${config.home.homeDirectory}/.config/hypr/scripts";
+     "$uS" = "${config.home.homeDirectory}/.config/hypr/UserScripts";
+     "$livewallpaper" = "mpvpaper";
+
+     exec-once = [
+       "$sD/Polkit-NixOS.sh"
+       "$sD/Hyprsunset.sh init"
+       "waypaper --restore"
+       "Throne"
+       "AyuGram"
+       "kitty"
+       "firefox"
+       "hypridle"
+       "wl-paste --type image --watch cliphist store"
+       "wl-paste --type text --watch cliphist store"
+       "hyprctl setcursor Bibata-Modern-Classic 24"
+       "waybar & swww-daemon"
+     ];
+  };
+}
