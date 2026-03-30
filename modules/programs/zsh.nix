6466e861de39cc9527581f520b5b3695befafa90
zumuvik <weterantip@gmail.com>
2026-03-30 23:58:11 +0300

    fix(zsh): use correct home-manager options for history

--- modules/programs/zsh.nix
+++ modules/programs/zsh.nix
@@ -5,30 +5,23 @@
     enable = true;
     enableCompletion = true;
     autocd = true;
-    histSize = 10000;
-    histFile = "$HOME/.zsh_history";
+    history = {
+      path = "$HOME/.zsh_history";
+      size = 10000;
+      save = 10000;
+      share = true;
+      expireDuplicatesFirst = true;
+      ignoreDups = true;
+      findNoDups = true;
+      ignoreSpace = true;
+    };
     initExtra = ''
-      # Shell options
       set -o vi
       set -o ignoreeof
       set -o correct
 
-      # History
-      HISTSIZE=10000
-      SAVEHIST=10000
-      HISTFILE=~/.zsh_history
-      SHARE_HISTORY=true
-      HIST_EXPIRE_DUPS_FIRST=true
-      HIST_IGNORE_DUPS=true
-      HIST_FIND_NO_DUPS=true
-      HIST_IGNORE_SPACE=true
-      APPEND_HISTORY=true
-      INC_APPEND_HISTORY=true
-
-      # Prompt
       PS1="%F{green}%n%f@%F{blue}%m%f %F{yellow}%~%f %% "
 
-      # Aliases
       alias ls='ls --color=auto'
       alias ll='ls -la'
       alias la='ls -a'
