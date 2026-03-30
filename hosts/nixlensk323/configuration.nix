534048cf34b82c63832fd5d9fc3b8400be35e699
zumuvik <weterantip@gmail.com>
2026-03-30 18:14:53 +0300

    feat: add laptop ssh key for remote build

--- hosts/nixlensk323/configuration.nix
+++ hosts/nixlensk323/configuration.nix
@@ -65,6 +65,7 @@
     shell = pkgs.fish;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEP3GKg44+5QOaTUj7kHMO9x4sMhShdVuK4NR1yMtleQ zumuvik@nixlensk323"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK9RWNYncLPCFQm4vcL0Ln3f8CG14g/JtUc42fPBjyJN laptop"
     ];
   };
 
