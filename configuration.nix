584c46facee60e015d30e78191365cbc55aba758
zumuvik <weterantip@gmail.com>
2026-03-30 22:51:16 +0300

    fonts: add jetbrains-mono and SF Pro Display (Bold, Regular)

--- configuration.nix
+++ configuration.nix
@@ -60,11 +60,33 @@
     packages = with pkgs; [
       nerd-fonts.jetbrains-mono
       nerd-fonts.iosevka
+      jetbrains-mono
       noto-fonts-cjk-serif
       noto-fonts
       noto-fonts-color-emoji
       dejavu_fonts
       liberation_ttf
+      (pkgs.stdenv.mkDerivation {
+        pname = "sf-pro-display";
+        version = "1.0";
+        srcs = [
+          (pkgs.fetchurl {
+            name = "sf-pro-bold.otf";
+            url = "https://raw.githubusercontent.com/MrVivekRajan/Hyprlock-Styles/main/Style-9/Fonts/SF%20Pro%20Display/SF%20Pro%20Display%20Bold.otf";
+            sha256 = "0pqv47piw79jglk641dripxmdpcgzr673kgiws9y7mmy9l9cxd8w";
+          })
+          (pkgs.fetchurl {
+            name = "sf-pro-regular.otf";
+            url = "https://raw.githubusercontent.com/MrVivekRajan/Hyprlock-Styles/main/Style-9/Fonts/SF%20Pro%20Display/SF%20Pro%20Display%20Regular.otf";
+            sha256 = "1kxj8hc9ckzgskwz78b9ijikbpy755808xzfllg9wbya01wd3d6z";
+          })
+        ];
+        sourceRoot = ".";
+        installPhase = ''
+          mkdir -p $out/share/fonts/opentype
+          cp *.otf $out/share/fonts/opentype/
+        '';
+      })
     ];
   };
 
