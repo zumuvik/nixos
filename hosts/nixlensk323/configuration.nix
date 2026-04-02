e97e4998a98f0913bd53577e8a1fec3e11013d65
zumuvik <weterantip@gmail.com>
2026-04-02 21:16:54 +0300

    loopse

--- hosts/nixlensk323/configuration.nix
+++ hosts/nixlensk323/configuration.nix
@@ -24,7 +24,7 @@
   networking.firewall.enable = true;
   networking.firewall.allowedTCPPorts = [ ];
   networking.firewall.allowedUDPPorts = [ ];
-
+  networking.firewall.checkReversePath = "loose";
   # ────────────────────────────────────────────────────────
   # Remote Builder
   # ────────────────────────────────────────────────────────
