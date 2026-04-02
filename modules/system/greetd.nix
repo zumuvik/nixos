4db9beb2674fb079316da9cc1b3655003c860e32
zumuvik <weterantip@gmail.com>
2026-04-02 11:44:57 +0300

    fix(greetd): use Hyprland-start instead of Hyprland for proper session setup

--- modules/system/greetd.nix
+++ modules/system/greetd.nix
@@ -5,7 +5,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd Hyprland --remember --remember-user-session";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd Hyprland-start --remember --remember-user-session";
         user = "greeter";
       };
     };
