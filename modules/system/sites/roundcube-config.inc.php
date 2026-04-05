44e6a0e4a13b788ca7d90fd7d2283c12def2ac5c
zumuvik <weterantip@gmail.com>
2026-04-05 20:11:33 +0300

    fix(roundcube): connect to localhost without SSL verification
    Roundcube connects locally so SSL cert mismatch on localhost.
    Use STARTTLS (port 143/587) instead of implicit SSL.

--- modules/system/sites/roundcube-config.inc.php
+++ modules/system/sites/roundcube-config.inc.php
@@ -1,10 +1,10 @@
 <?php
 $config = array();
 $config['db_dsnw'] = 'mysql://roundcube:roundcube_password@localhost/roundcube';
-$config['default_host'] = 'ssl://localhost';
-$config['default_port'] = 993;
-$config['smtp_server'] = 'ssl://localhost';
-$config['smtp_port'] = 465;
+$config['default_host'] = 'localhost';
+$config['default_port'] = 143;
+$config['smtp_server'] = 'localhost';
+$config['smtp_port'] = 587;
 $config['smtp_user'] = '%u';
 $config['smtp_pass'] = '%p';
 $config['support_url'] = '';
@@ -12,3 +12,15 @@ $config['product_name'] = 'Mail Samolensk';
 $config['plugins'] = array('archive', 'zipdownload');
 $config['skin'] = 'elastic';
 $config['language'] = 'ru_RU';
+$config['imap_conn_options'] = array(
+    'ssl' => array(
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+    ),
+);
+$config['smtp_conn_options'] = array(
+    'ssl' => array(
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+    ),
+);
