165b88785aaeeef734ad8b282d7e0935557ce7c9
zumuvik <weterantip@gmail.com>
2026-04-09 00:03:10 +0300

    Оптимизация zsh конфигурации и удаление дублирующихся настроек

--- configuration.nix
+++ configuration.nix
@@ -99,13 +99,9 @@
     max-substitution-jobs = 32;
     substituters = [
       "https://cache.nixos.org"
-      "https://nix-community.cachix.org"
-      "https://cache.nixvim.org"
     ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "cache.nixvim.org-1:RGi+DSkOPO23SnruXW/8xU+717xPc2+JOqUXBBZa598="
     ];
   };
 
