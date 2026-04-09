2d70755c2999c5942a21bd54eb38758969d8bbcf
zumuvik <weterantip@gmail.com>
2026-04-09 23:56:50 +0300

    micro: fix init.lua generation

--- modules/programs/micro.nix
+++ modules/programs/micro.nix
@@ -20,33 +20,31 @@ let
     filemanager.openonstart = true;
     filemanager.width = 20;
   };
-
-  xdg.configFile."micro/init.lua".text = ''
-    local micro = import("micro")
-    local config = import("config")
-
-    -- Close empty buffer on startup if filemanager is enabled
-    micro.Init(function()
-      if config.GetGlobalOption("filemanager.openonstart") then
-        -- Wait a moment for plugins to load
-        local function closeEmpty()
-           for _, pane in ipairs(micro.Panes) do
-             local buf = pane.Buf
-             if buf.Path == "" and buf:Modified() == false then
-               pane:Close()
-             end
-           end
-        end
-        micro.AddTimer(100):Start(closeEmpty)
-      end
-    end)
-  '';
 in
 {
   programs.micro = {
     enable = true;
     settings = microSettings;
   };
+
+  home.file.".config/micro/init.lua".text = ''
+    local micro = import("micro")
+    local config = import("config")
+
+    micro.Init(function()
+      if config.GetGlobalOption("filemanager.openonstart") then
+        local function closeEmpty()
+          for _, pane in ipairs(micro.Panes) do
+            local buf = pane.Buf
+            if buf.Path == "" and buf:Modified() == false then
+              pane:Close()
+            end
+          end
+        end
+        micro.AddTimer(100):Start(closeEmpty)
+      end
+    end)
+  '';
 
   home.activation.installMicroPlugins = lib.hm.dag.entryAfter ["writeBoundary"] ''
     ${pkgs.micro}/bin/micro -plugin install fzf snippets pony misspell crystal editorconfig go jump autofmt detectindent palettero monokai-dark runit fish bookmark joinLines manipulator aspell wakatime bounce quickfix jlabbrev wc gotham-colors cheat nordcolors quoter
