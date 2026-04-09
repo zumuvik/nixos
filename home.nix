1e94221d6120e5c61e9e757838fe3187eaffbae0
zumuvik <weterantip@gmail.com>
2026-04-09 23:42:29 +0300

    Move micro config to modules/programs/micro.nix

--- home.nix
+++ home.nix
@@ -105,32 +105,4 @@
     enable = true;
     package = pkgs.ghostty;
   };
-
-  programs.micro = {
-    enable = true;
-    settings = {
-      autosuccess = true;
-      clipboard = "terminal";
-      colorscheme = "catppuccin-macchiato";
-      cursorline = true;
-      mkparents = true;
-      mouse = true;
-      rmtrailingws = true;
-      savecursor = true;
-      saveundo = true;
-      scrollbar = false;
-      statusline = true;
-      tabsize = 4;
-      tabstospaces = true;
-      softwrap = true;
-      sucmd = "sudo";
-
-      filemanager.openonstart = true;
-      filemanager.width = 20;
-      linter = true;
-      autoclose = true;
-      lsp.autocomplete = true;
-      lsp.formatOnSave = true;
-    };
-  };
 }
