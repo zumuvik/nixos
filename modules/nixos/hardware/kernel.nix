3d7d5193d0279b26607730a0ab67ec0375680237
zumuvik <weterantip@gmail.com>
2026-04-27 20:58:19 +0300

    fix: use correct path for chaotic kernel

--- modules/nixos/hardware/kernel.nix
+++ modules/nixos/hardware/kernel.nix
@@ -21,7 +21,7 @@
       boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest;
     })
     (lib.mkIf config.my.hardware.kernel-cachy-bore.enable {
-      boot.kernelPackages = pkgs.linuxPackages_cachyos;
+      boot.kernelPackages = pkgs.chaotic.linuxPackages_cachyos;
     })
   ];
 }
