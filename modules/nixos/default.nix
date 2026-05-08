8a33c729db4ff81e630935d2b80fcc4ff4e3252f
zumuvik <weterantip@gmail.com>
2026-05-08 23:01:17 +0300

    fix: move nix settings to modules/nixos/default.nix

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,4 +1,16 @@
 { ... }: {
+  nix.settings = { 
+    substituters = [ 
+      "https://cache.nixos.org" 
+      "https://xddxdd.cachix.org" 
+      "https://nix-cachyos-kernel.cachix.org" 
+    ]; 
+    trusted-public-keys = [ 
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" 
+      "xddxdd.cachix.org-1:ay1HJyNDYmlSwj5NXQG065C8LfoqqKaTNCyzeixGjf8=" 
+      "nix-cachyos-kernel.cachix.org-1:709cn79S9L2XYD6B2S6mE6fF8n+yH5/S12CfS+J4mRM=" 
+    ]; 
+  };
   imports = [
     ./services/nginx
     ./services/mailserver
