a42bad714342259a52b344b06273e41301b0ce9b
zumuvik <weterantip@gmail.com>
2026-05-09 10:23:58 +0300

    fix: switch to programs.vscodium

--- modules/home/programs/vscodium.nix
+++ modules/home/programs/vscodium.nix
@@ -1,6 +1,6 @@
 { my, lib, pkgs, ... }:{
   config = lib.mkIf my.profiles.desktop.enable {
-    programs.vscode = {
+    programs.vscodium = {
       enable = true;
       package = pkgs.vscodium.fhs;
     };
