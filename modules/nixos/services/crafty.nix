ee348d77f4b30d852f90548b061b0f48a04be708
zumuvik <zumuvik@local>
2026-05-26 00:48:17 +0300

    fix: for fix

--- modules/nixos/services/crafty.nix
+++ modules/nixos/services/crafty.nix
@@ -84,9 +84,10 @@ in
         enableACME = true;
         forceSSL = true;
         locations."/" = {
-          proxyPass = "http://127.0.0.1:8000";
+          proxyPass = "https://127.0.0.1:${toString cfg.port}";
           proxyWebsockets = true;
           extraConfig = ''
+            proxy_ssl_verify off;
             proxy_set_header Host $host;
             proxy_set_header X-Real-IP $remote_addr;
             proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
