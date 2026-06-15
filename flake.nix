41ffcce8693e322e956557925e8132e0cf62becf
zumuvik <weterantip@gmail.com>
2026-06-15 19:39:59 +0300

    fix: глобальное применение бинарного кэша для ядер CachyOS через nixConfig

--- flake.nix
+++ flake.nix
@@ -1,6 +1,20 @@
 {
   description = "NixOS configuration for zumuvik's machines";
 
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.garnix.io"
+      "https://xddxdd.cachix.org"
+      "https://nix-cachyos-kernel.cachix.org"
+      "https://attic.xuyh0120.win/lantian"
+    ];
+    extra-trusted-public-keys = [
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      "xddxdd.cachix.org-1:ay1HJyNDYmlSwj5NXQG065C8LfoqqKaTNCyzeixGjf8="
+      "nix-cachyos-kernel.cachix.org-1:709cn79S9L2XYD6B2S6mE6fF8n+yH5/S12CfS+J4mRM="
+      "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
+    ];
+  };
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -73,6 +87,18 @@
 
           ({ ... }: {
             nixpkgs.overlays = [ inputs.nix-cachyos-kernel.overlays.pinned ];
+            nix.settings = {
+              substituters = [ 
+                "https://xddxdd.cachix.org"
+                "https://nix-cachyos-kernel.cachix.org"
+                "https://attic.xuyh0120.win/lantian" 
+              ];
+              trusted-public-keys = [ 
+                "xddxdd.cachix.org-1:ay1HJyNDYmlSwj5NXQG065C8LfoqqKaTNCyzeixGjf8="
+                "nix-cachyos-kernel.cachix.org-1:709cn79S9L2XYD6B2S6mE6fF8n+yH5/S12CfS+J4mRM="
+                "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=" 
+              ];
+            };
           })
 
           ( { config, ... }: {
